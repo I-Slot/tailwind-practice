30f341393ce92bfaba7caf6890b20d38c4ad1ff8
github_i-slot <ismailadesholaharun@gmail.com>
2023-07-26 20:18:07 +0100

    Tailwind practice

---
+++ tailwind-proj/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      {/* <section className="h-screen bg-twitter-blue flex items-center justify-center m-12 text-center text-3xl p-12 md:p-14 lg:p-16 xl:p20">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          perferendis asperiores, quidem harum quod maxime beatae corporis, iste
+          magni sequi eius facere eveniet eum hic fugiat sapiente distinctio
+          neque recusandae labore voluptas nulla. Nemo voluptate neque est autem
+          quibusdam suscipit optio placeat debitis nobis quasi? Fugiat rem amet
+          voluptatem voluptates.
+        </p>
+      </section> */}
+
+      <div className="card rounded-none text-red-600">This is the card.</div>
+    </div>
+  );
+};
+
+export default App;
